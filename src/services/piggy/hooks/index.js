94dcaa2a21e441f214aa851ddd6a526cd4efcc33
EsterKais <kaisester91@gmail.com>
2016-11-07 18:04:56 +0100

    Hooks Gen

--- src/services/piggy/hooks/index.js
+++ src/services/piggy/hooks/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const createPiggy = require('./create-piggy');
+
 const globalHooks = require('../../../hooks');
 const hooks = require('feathers-hooks');
 const auth = require('feathers-authentication').hooks;
@@ -12,7 +14,7 @@ exports.before = {
   ],
   find: [],
   get: [],
-  create: [],
+  create: [createPiggy()],
   update: [],
   patch: [],
   remove: []
