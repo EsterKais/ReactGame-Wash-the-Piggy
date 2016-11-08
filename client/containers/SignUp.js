c769c277b5df185546c3163f1201f7b1692b4e62
lauradelar <lrdela.rie@gmail.com>
2016-11-08 19:50:23 +0100

    start on signup form

--- client/containers/SignUp.js
+++ client/containers/SignUp.js
@@ -5,9 +5,11 @@ class SignUp extends Component {
   submitForm(event){
     event.preventDefault()
 
-    const { name } = this.refs
+    const { name, email } = this.refs
 
     console.log('name: ', name.value )
+    console.log('email: ', email.value )
+
   }
 
 
@@ -17,9 +19,22 @@ class SignUp extends Component {
         <form onSubmit={ this.submitForm.bind(this) }>
           <h1> Sign Up to Wash this Piggy </h1>
           <div className='input'>
-            <input type="name" ref="name" />
-          </div>
+            <input type="name" ref="name" placeholder="What's your name?" />
+          </div><br />
 
+          <div className='input'>
+            <input type="email" ref="email" placeholder="What's your email?" />
+          </div><br />
+          <div className='input'>
+            <input type='password' ref="password" placeholder="Set a password" />
+          </div><br />
+          <div className='input'>
+            <input type='password' ref="passwordConfirmation" placeholder="Confirm your password" />
+          </div><br />
+          <div className='controls'>
+            <button type='submit'>Sign Up</button>
+            <button label="link">Already Signed Up?</button>
+          </div>
         </form>
       </div>
     )
